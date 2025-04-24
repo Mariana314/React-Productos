6e8e07042d90b0d845042efe9dd5c7f87f6fef27
dahiannaroman <karendahiannaromanbarrios>
2025-04-24 14:01:35 -0500

    Estilos en Tailwind para ProductCard

--- src/Components/Layouts/ProductCard/ProductCard.jsx
+++ src/Components/Layouts/ProductCard/ProductCard.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 export const ProductCard = ({ title, image, price }) => {
   return (
-    <div>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>${price}</p>
+    <div className="w-64 bg-white rounded-2xl shadow-md p-4 flex flex-col items-center text-center transition-transform hover:scale-105">
+      <img
+        src={image}
+        alt={title}
+        className="w-32 h-32 object-contain mb-4"
+      />
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
+      <p className="text-emerald-300 font-bold text-md">${price}</p>
     </div>
   )
 }
